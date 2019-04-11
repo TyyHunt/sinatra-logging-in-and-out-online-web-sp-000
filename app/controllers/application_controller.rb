49fdf6ee42e5d90975d7c26731f4760c22fe44f0
Tyler Hunt <tyhunt04@gmail.com>
2019-04-11 16:49:43 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,7 +13,7 @@ class ApplicationController < Sinatra::Base
   post '/login' do
     @user = User.find_by(username: params[:username])
     if @user
-      params[:user_id] = @user.id 
+      params[:user_id] = @user.id
       redirect to '/account'
   end
 
